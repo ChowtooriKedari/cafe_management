02ceb9f601f96b8061121423b1e489d448cf7096
ChowtooriKedari <chowtoorikedari@gmail.com>
2020-05-05 18:01:55 +0530

    Reports

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_04_063211) do
+ActiveRecord::Schema.define(version: 2020_05_05_072903) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,4 +75,5 @@ ActiveRecord::Schema.define(version: 2020_05_04_063211) do
     t.boolean "is_clerk"
     t.string "password_digest"
   end
+
 end
